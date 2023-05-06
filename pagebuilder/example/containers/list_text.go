73c3907805fee0fc90ffc148845c13027afc05c0
Eula Zeng <eula@theplant.jp>
2023-05-06 15:41:47 +0800

    add img

--- pagebuilder/example/containers/list_text.go
+++ pagebuilder/example/containers/list_text.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/anshiii/playground-admin/media/media_library"
 	"github.com/anshiii/playground-admin/pagebuilder"
 	"github.com/anshiii/playground-admin/presets"
 	"github.com/iancoleman/strcase"
@@ -15,12 +16,13 @@ import (
 	"gorm.io/gorm"
 )
 
-type TextListItem struct {
+type TextItem struct {
 	Text  string
 	Title string
+	Image media_library.MediaBox `sql:"type:text;"`
 }
 
-type TextListItems []*TextListItem
+type TextListItems []*TextItem
 type TextList struct {
 	ID       uint
 	AnchorID string
@@ -31,7 +33,7 @@ type TextList struct {
 }
 
 func (*TextList) TableName() string {
-	return "text_list_content"
+	return "text_list_content_but_with_image"
 }
 
 func (this TextListItems) Value() (driver.Value, error) {
@@ -58,28 +60,28 @@ func RegisterTextListContainer(pageBuilder *pagebuilder.Builder, db *gorm.DB) {
 	model := container.Model(&TextList{})
 	editable := model.Editing("AnchorID", "Title", "Items")
 
-	itemField := pageBuilder.GetPresetsBuilder().NewFieldsBuilder(presets.WRITE).Model(&TextListItem{}).Only("Text", "Title")
+	itemField := pageBuilder.GetPresetsBuilder().NewFieldsBuilder(presets.WRITE).Model(&TextItem{}).Only("Text", "Title", "Image")
 
 	editable.Field("Items").Nested(itemField, &presets.DisplayFieldInSorter{Field: "Text"})
 }
 
 func ListBody(props *TextList, input *pagebuilder.RenderInput) HTMLComponent {
+	var wrapEle = Ul()
+	for _, item := range props.Items {
+		var contentEle = ListItemBody(item)
+		wrapEle.AppendChildren(contentEle)
+	}
 	var body = ContainerWrapper(fmt.Sprintf(inflection.Plural(strcase.ToKebab("TextList"))+"_%v", props.ID), props.AnchorID,
 		"container-list_content container-lottie", "", "", "", "", true, true, true, "",
-		Ul(
-			ListItemBody(props.Items),
-		))
+		wrapEle)
 	return body
 }
 
-func ListItemBody(items []*TextListItem) HTMLComponent {
-	var itemsWrap *HTMLTagBuilder = Li().Class("container-list_content-grid")
-	for _, item := range items {
-		var itemEle HTMLComponent = Div(
-			P().Text(item.Title).Class("text-list-item-title"),
-			Span(item.Text).Class("text-list-item-text"),
-		)
-		itemsWrap.AppendChildren(itemEle)
-	}
-	return itemsWrap
+func ListItemBody(item *TextItem) HTMLComponent {
+	var itemEle HTMLComponent = Li(
+		Img(item.Image.Url),
+		P().Text(item.Title).Class("text-list-item-title"),
+		Span(item.Text).Class("text-list-item-text"),
+	)
+	return itemEle
 }
